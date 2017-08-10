b426819e59c0a76204eab8c32e0d0612cb2c7857
licc168 <liccwork@126.com>
2017-08-10 15:37:45 +0800

    springBoot rabbitMq demo

---
+++ rabbitMq/src/test/java/com/licc/rabbitMq/RabbitMqApplicationTests.java
@@ -0,0 +1,21 @@
+package com.licc.rabbitMq;
+
+import com.licc.rabbitMq.producer.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitMqApplicationTests {
+
+	@Autowired
+	private Sender sender;
+	@Test
+	public void hello() throws Exception {
+		sender.send();
+	}
+
+}
